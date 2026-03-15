0d7e3c3f671511cc53491d70c72c7cc392b5d7f6
Nitheesh14125 <s.nitheesh14125@gmail.com>
2026-03-15 14:08:21 +0530

    Valid palindrome or not leet code varient covert and check

---
+++ FInal_challenge_Going_to_make_it/WEEK - 1/Array_Advanced_Two_pointers/valid_palindrome_leetcode.py
@@ -0,0 +1,17 @@
+def valid_palindrome(s):
+    s = s.lower()
+    result = ""
+    for i in range(len(str)):
+        if s[i].isalnum():
+            result = result + s[i]
+    left = 0 
+    right = len(result) - 1
+    while left < right :
+        if result[left] != result[right]:
+            return False
+        left = left + 1
+        right = right - 1   
+    return True 
+    
+str = "A man, a plan, a canal: Panama"
+print(valid_palindrome(str))
